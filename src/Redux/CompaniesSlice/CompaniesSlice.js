68c9f919a3f12f0aa7019e27dec07b0aa2df2888
HFG43lear <hernanguemes@hotmail.com>
2023-08-01 18:38:19 -0300

    feat: finish market view

---
+++ src/Redux/CompaniesSlice/CompaniesSlice.js
@@ -0,0 +1,44 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const url = 'https://financialmodelingprep.com/api/v3/quote/AAPL,MSFT,AMZN,GOOGL,FB,TSLA,BRK.B,JNJ,JPM,V,PG,MA,UNH,NVDA,HD,PYPL,ADBE,CRM,NFLX,BAC,INTC,CSCO,XOM,CMCSA,CVX,PEP,PFE,ABT,ABBV,LLY,NKE,COST,DHR,NEE,UNP,HON,AMD,C,ACN,LIN,MRK,ORCL,WMT,UPS,MCD,BMY,AMGN,PM,TMO,DIS,LMT,MO,GD,MDT,BLK,BKNG,GS,AXP,LRCX,CHTR,RTX,SPGI,BDX,CCI,NOC,ISRG,PLD,ANTM,SBUX,CI,SCHW,MCO,D,ADP,ILMN,ZTS,REGN,SYK,TJX,BIIB,ADSK,VRTX,CME,NOW,IBM,HCA,SO,FIS,ATVI,CL,GS,ANTM,MS,ZTS?&apikey=';
+const apiKey = '66e98217e2416f23a2ebb8309751840';
+
+const initialState = {
+  companies: [{ company_symbol: 'aaa', company_name: 'aaa', company_price: 10 }],
+  status: 'idle',
+};
+
+export const getCompaniesData = createAsyncThunk(
+  'statistics/getCompaniesData',
+  async () => {
+    try {
+      const response = await axios.get(`${url}${apiKey}`);
+      return response.data;
+    } catch (error) {
+      return 'Something went wrong, please try again';
+    }
+  },
+);
+
+const companiesSlice = createSlice({
+  name: 'companies',
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(getCompaniesData.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(getCompaniesData.fulfilled, (state, action) => {
+        state.status = 'succeded';
+        state.companies = action.payload;
+      })
+      .addCase(getCompaniesData.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default companiesSlice.reducer;
--- src/components/CompaniesList.js
+++ src/components/CompaniesList.js
@@ -1,6 +1,31 @@
-const CompaniesList = () => (
-  <>
-    <div>Companies</div>
-  </>
-);
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { getCompaniesData } from '../Redux/CompaniesSlice/CompaniesSlice';
+
+function CompaniesList() {
+  const companies = useSelector((state) => state.companies.companiesData);
+  const status = useSelector((state) => state.companies.status);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (status === 'idle') {
+      dispatch(getCompaniesData());
+    }
+  }, [dispatch, status]);
+
+  return (
+    <>
+      <article>
+        {companies.map((company) => (
+          <div key={company.company_symbol}>
+            <span>{company.company_name}</span>
+            <span>{company.company_price}</span>
+            <span>Prueba</span>
+          </div>
+        ))}
+      </article>
+    </>
+  );
+}
+
 export default CompaniesList;
